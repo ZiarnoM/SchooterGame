01d09dcab03ef4d0a2a3a863b0a112ce847e4398
Michal <zar.michal@wp.pl>
2022-04-10 22:25:13 +0200

    fix player collision

--- Player.cpp
+++ Player.cpp
@@ -7,20 +7,22 @@
 #include <math.h>
 
 void Player::initVariables() {
-    this->movementSpeed = 10.f;
+    this->movementSpeed = 5.f;
 }
 
 void Player::initPlayer() {
     this->initTexture();
     playerSprite.scale(sf::Vector2f(0.1, 0.1));
 }
+
 void Player::initTexture() {
     if (!this->texture.loadFromFile("../textures/player.png")) {
         std::cout << "Could not load player texture" << std::endl;
     }
     playerSprite.setTexture(texture);
-    this->playerSprite.setOrigin(texture.getSize().x/2,texture.getSize().y/2);
+    this->playerSprite.setOrigin(texture.getSize().x / 2, texture.getSize().y / 2);
 }
+
 Player::Player(float x, float y) {
     this->playerSprite.setPosition(x, y);
     this->initVariables();
@@ -59,46 +61,39 @@ void Player::updateInput() {
 void Player::updateWindowBoundsCollision(const sf::RenderTarget *target) {
     //left
     if (this->playerSprite.getGlobalBounds().left <= 0.f) {
-        this->playerSprite.setPosition(0.f, this->playerSprite.getGlobalBounds().top);
+        this->playerSprite.move(this->movementSpeed, 0.f);
     }
     //right
     if (this->playerSprite.getGlobalBounds().left + this->playerSprite.getGlobalBounds().width >= target->getSize().x) {
-        this->playerSprite.setPosition(
-                target->getSize().x - this->playerSprite.getGlobalBounds().width,
-                this->playerSprite.getGlobalBounds().top
-        );
+        this->playerSprite.move(-this->movementSpeed, 0.f);
     }
     //top
     if (this->playerSprite.getGlobalBounds().top <= 0.f) {
-        this->playerSprite.setPosition(this->playerSprite.getGlobalBounds().left, 0.f);
+        this->playerSprite.move(0.f, this->movementSpeed);
     }
     //bottom
     if (this->playerSprite.getGlobalBounds().top + this->playerSprite.getGlobalBounds().height >= target->getSize().y) {
-        this->playerSprite.setPosition(
-                this->playerSprite.getGlobalBounds().left,
-                target->getSize().y - this->playerSprite.getGlobalBounds().height
-        );
+        this->playerSprite.move(0.f, -this->movementSpeed);
     }
 }
 
 void Player::rotatePlayer(sf::Vector2i position) {
     sf::FloatRect playerBounds = this->playerSprite.getGlobalBounds();
     // exit if mouse is over player
-    if (!((position.x<=playerBounds.left ||position.x>=playerBounds.left+playerBounds.width)||
-    (position.y<=playerBounds.top ||position.y>=playerBounds.top+playerBounds.height))) {
+    if (!((position.x <= playerBounds.left || position.x >= playerBounds.left + playerBounds.width) ||
+          (position.y <= playerBounds.top || position.y >= playerBounds.top + playerBounds.height))) {
         return;
     }
-        const float PI = 3.14159265;
+    const float PI = 3.14159265;
 
-        float dx = playerBounds.left + playerBounds.width / 2 - position.x;
-        float dy = playerBounds.top + playerBounds.height / 2 - position.y;
+    float dx = playerBounds.left + playerBounds.width / 2 - position.x;
+    float dy = playerBounds.top + playerBounds.height / 2 - position.y;
 
-        float rotation = (atan2(dy, dx)) * 180 / PI;
-//        std::cout << this->playerSprite.getOrigin().x << "\n";
-//        this->playerSprite.setOrigin(playerBounds.width/2,playerBounds.height/2);
-        this->playerSprite.setRotation(rotation - 90);
-
+    float rotation = (atan2(dy, dx)) * 180 / PI;
+    this->playerSprite.setRotation(rotation);
 }
+
+
 
 
 
